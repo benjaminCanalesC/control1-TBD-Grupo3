36059593ce653c2fd1998eda2de5403bbf3052a5
AracelyC1 <aracely.castro@usach.cl>
2023-09-11 13:29:57 -0300

    script poblar horario
    Pequeño script para poblar la tabla horario. Observaciones:
    Date sigue el formato YYYY-MM-DD
    Time sigue el formato hh:mm:ss, su tiempo en el formato de 24 horas

---
+++ script poblar horario.sql
@@ -0,0 +1,13 @@
+
+insert into horario (idHorario, fecha, hora) values (1, '2023-01-25', '12:32');
+insert into horario (idHorario, fecha, hora) values (2, '2023-05-06', '12:20');
+insert into horario (idHorario, fecha, hora) values (3, '2023-12-07', '07:34');
+insert into horario (idHorario, fecha, hora) values (4, '2023-11-10', '22:23');
+insert into horario (idHorario, fecha, hora) values (5, '2023-04-12', '12:43');
+insert into horario (idHorario, fecha, hora) values (6, '2023-05-28', '09:26');
+insert into horario (idHorario, fecha, hora) values (7, '2023-03-12', '10:43');
+insert into horario (idHorario, fecha, hora) values (8, '2023-12-25', '14:12');
+insert into horario (idHorario, fecha, hora) values (9, '2023-01-04', '15:56');
+insert into horario (idHorario, fecha, hora) values (10, '2023-07-22', '18:13');
+insert into horario (idHorario, fecha, hora) values (11, '2023-06-02', '19:32');
+insert into horario (idHorario, fecha, hora) values (12, '2023-02-01', '10:23');
