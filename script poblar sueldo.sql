071b88ce625a1b085b2a4f30bfabba0aa827ad68
AracelyC1 <aracely.castro@usach.cl>
2023-09-11 13:20:19 -0300

    script poblar sueldo
    Se agrega un script pequeño para
    la tabla sueldo

---
+++ script poblar sueldo.sql
@@ -0,0 +1,14 @@
+
+insert into sueldo (idSueldo, monto) values (1, 23000);
+insert into sueldo (idSueldo, monto) values (2, 14500);
+insert into sueldo (idSueldo, monto) values (3, 4550);
+insert into sueldo (idSueldo, monto) values (4, 10000);
+insert into sueldo (idSueldo, monto) values (5, 15000);
+insert into sueldo (idSueldo, monto) values (6, 2000);
+insert into sueldo (idSueldo, monto) values (7, 1350);
+insert into sueldo (idSueldo, monto) values (8, 2300);
+insert into sueldo (idSueldo, monto) values (9, 5000);
+insert into sueldo (idSueldo, monto) values (10, 12300);
+insert into sueldo (idSueldo, monto) values (11, 3800);
+insert into sueldo (idSueldo, monto) values (12, 4200);
+
