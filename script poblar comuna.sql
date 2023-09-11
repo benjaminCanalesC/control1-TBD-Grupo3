14dbf6defbdcce6392b0ece3a47d0111ef51e601
AracelyC1 <aracely.castro@usach.cl>
2023-09-11 13:15:35 -0300

    script poblar comuna
    Se agrega un script perqueño para
    la tabla comuna

---
+++ script poblar comuna.sql
@@ -0,0 +1,12 @@
+
+
+insert into comuna (idComuna, nombreComuna) values (1, 'Santiago');
+insert into comuna (idComuna, nombreComuna) values (2, 'Renca');
+insert into comuna (idComuna, nombreComuna) values (3, 'Las Condes');
+insert into comuna (idComuna, nombreComuna) values (4, 'Maipú');
+insert into comuna (idComuna, nombreComuna) values (5, 'La Florida');
+insert into comuna (idComuna, nombreComuna) values (6, 'Tocopilla');
+insert into comuna (idComuna, nombreComuna) values (7, 'Copiapó');
+insert into comuna (idComuna, nombreComuna) values (8, 'Valparaíso');
+insert into comuna (idComuna, nombreComuna) values (9, 'Quintero');
+insert into comuna (idComuna, nombreComuna) values (10, 'Rancagua');
