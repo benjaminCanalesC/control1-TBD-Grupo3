f31117f65d263eb325f551f72e1ed873c8a138d8
Mieres7 <111611599+Mieres7@users.noreply.github.com>
2023-09-11 21:09:01 -0300

    Update script poblar cliente.sql

--- script poblar cliente.sql
+++ script poblar cliente.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS public.cliente(
     apellidoCliente TEXT COLLATE pg_catalog."default",
     idComuna INTEGER);
 
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (1, 'Víctor', 'Flores', 1);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (2, 'Cintia', 'Romero', 2);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (3, 'Sergio', 'Alvarez', 3);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (4, 'Claudia', 'Sanchez', 4);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (5, 'Julio', 'Gonzalez', 5);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (6, 'Horacio', 'Gimenez', 6);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (7, 'Susana', 'Fernandez', 7);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (8, 'Julio', 'Gutierrez', 8);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (9, 'Liliana', 'Rodriguez', 9);
-insert into cliente (idCliente, nombreCliente, apellidoCliente, idComuna) values (10, 'Juan', 'Benitez', 10);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Víctor', 'Flores', 1);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Cintia', 'Romero', 2);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Sergio', 'Alvarez', 3);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Claudia', 'Sanchez', 4);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Julio', 'Gonzalez', 5);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Horacio', 'Gimenez', 6);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Susana', 'Fernandez', 7);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Julio', 'Gutierrez', 8);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Liliana', 'Rodriguez', 9);
+insert into cliente (nombre_cliente, apellido_cliente, id_comuna) values ('Juan', 'Benitez', 10);
