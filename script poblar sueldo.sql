e3996102c7b2afff75e74b31ec9f5ec3ee9d5174
Benjamín Canales Cempe <135075287+benjaminCanalesC@users.noreply.github.com>
2023-09-11 21:09:12 -0300

    Update script poblar sueldo.sql

--- script poblar sueldo.sql
+++ script poblar sueldo.sql
@@ -1,14 +1,12 @@
-
-insert into sueldo (idSueldo, monto) values (1, 23000);
-insert into sueldo (idSueldo, monto) values (2, 14500);
-insert into sueldo (idSueldo, monto) values (3, 4550);
-insert into sueldo (idSueldo, monto) values (4, 10000);
-insert into sueldo (idSueldo, monto) values (5, 15000);
-insert into sueldo (idSueldo, monto) values (6, 2000);
-insert into sueldo (idSueldo, monto) values (7, 1350);
-insert into sueldo (idSueldo, monto) values (8, 2300);
-insert into sueldo (idSueldo, monto) values (9, 5000);
-insert into sueldo (idSueldo, monto) values (10, 12300);
-insert into sueldo (idSueldo, monto) values (11, 3800);
-insert into sueldo (idSueldo, monto) values (12, 4200);
-
+insert into sueldo (monto) values (23000);
+insert into sueldo (monto) values (14500);
+insert into sueldo (monto) values (4550);
+insert into sueldo (monto) values (10000);
+insert into sueldo (monto) values (15000);
+insert into sueldo (monto) values (2000);
+insert into sueldo (monto) values (1350);
+insert into sueldo (monto) values (2300);
+insert into sueldo (monto) values (5000);
+insert into sueldo (monto) values (12300);
+insert into sueldo (monto) values (3800);
+insert into sueldo (monto) values (4200);
